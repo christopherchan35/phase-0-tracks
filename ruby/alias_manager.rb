57f7e80ef55b742c1dbbe9ae4ee1a562a64e8dd4
Christopher Chan <christopherchan35@gmail.com>
2016-07-24 12:58:10 -0700

    Add pseudocode for swapping spy name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+# 5.5 Manipulating Strings with Iteration
+
+# Release 0
+# Take in string of full name and split by white space
+# Swap the place of the 2 elements with the help of a temporary variable
+# Create an array of vowels and a separate array of consonants
+# Change each vowel to the next vowel in the array and each consonant to the next one
+
