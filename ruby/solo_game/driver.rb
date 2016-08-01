6f2bc3f6154b3a2b05fc400484f6c85459d89183
Christopher Chan <christopherchan35@gmail.com>
2016-07-31 17:44:39 -0700

    Able to replace word with correct letters, but win condition not working

--- ruby/solo_game/driver.rb
+++ ruby/solo_game/driver.rb
@@ -1,3 +1,29 @@
 require_relative 'game.rb'
 
 game = Game.new
+
+puts "Welcome to the Word-Guessing Game!"
+puts "Player 1, please enter a word: "
+game.word = gets.chomp
+#p game.word
+puts "\e[H\e[2J"
+puts "Player 2, enter a letter: "
+game.guess_letter = gets.chomp
+game.generate_answer(game.word)
+game.guess
+game.add_to_guesses
+
+while game.guess_count != game.word.length
+  puts "Enter a letter: "
+  game.guess_letter = gets.chomp
+  game.guess
+  game.add_to_guesses
+end
+
+if game.guess_count == game.word.length
+  if !@game_won
+    puts "Better luck next time"
+  else
+    puts "Congrats!"
+  end
+end
--- ruby/solo_game/game.rb
+++ ruby/solo_game/game.rb
@@ -1,8 +1,46 @@
 class Game
-  attr_accessor :word
+  attr_accessor :word, :guess_letter,
+    :guess_count
 
   def initialize
-    p "success"
+    @word = word
+    @guess_count = 0
+    @guess_letter = guess_letter
+    @guesses = []
+    @game_won = false
+    @answer = []
+  end
+
+  def generate_answer(answer)
+    answer.length.times { @answer << '-'}
+  end
+
+  def guess()
+    if !@game_won
+      i = 0
+      @word.each_char do |letter|
+        if @guess_letter == letter
+          @answer[i] = letter
+        else
+        end
+        i += 1
+      end
+
+      if @guesses.include?(@guess_letter)
+        p 'You tried that already...'
+      else
+        @guess_count += 1
+      end
+    else
+      @game_won = true
+    end
+
+    print @answer.join('')
+    puts ' '
+  end
+
+  def add_to_guesses
+    @guesses << @guess_letter
   end
 
 end
