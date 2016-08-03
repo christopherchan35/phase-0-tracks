22e853076ce225ebba1e8f4b59f63ceea14fb7e6
Christopher Chan <christopherchan35@gmail.com>
2016-08-03 10:47:33 -0700

    Added test for win condition

--- ruby/solo_game/game_spec.rb
+++ ruby/solo_game/game_spec.rb
@@ -16,11 +16,16 @@ describe Game do
     expect(@word.length).to eq 7
   end
 
-=begin
+  it "switches win condition to true when dashes are gone" do
+    @answer = ['u', 'n', 'i', 'c', 'o', 'r', 'n']
+    @game.check
+    expect(@game.game_won).to eq true
+  end
+
   it "adds each guess into the guesses array" do
+    @game.add_to_guesses
     @guess_letter = 'u'
     @game.add_to_guesses
     expect(@guesses).to eq ['u']
   end
-=end
 end
