a1f44067110d1b443e52615d27386de362878e41
Christopher Chan <christopherchan35@gmail.com>
2016-08-04 23:06:55 -0700

    Created a function that checks if 2 hashes have matching key-value pair

--- js/algos.js
+++ js/algos.js
@@ -18,3 +18,37 @@ console.log(longest(["long phrase", "longest phrase", "longer phrase"]));
 console.log(longest(["a", "aa", "aaa"]));
 
 console.log(longest(["this should be the longest word in the string", "this is not", "neither is this"]));
+
+// release 1
+// 1. create a function that takes in 2 objects
+// 2. compare the objects (which will be hashes) to see if they both have the same key-value pair
+// 3. if there is a match then return true. otherwise return false
+
+function find_match(hash1, hash2){
+  var array1 = Object.keys(hash1);
+  var array2 = Object.keys(hash2);
+
+  for(var i = 0; i < array1.length; i++){
+    for(var j = 0; j < array2.length; j++){
+      if(array1[i] == array2[j]){
+        if(hash1[array1[i]] == hash2[array2[j]]){
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+var males = {name: "Bob", age: 35};
+var females = {name: "Bertha", age: 35};
+
+console.log(find_match(males, females));
+
+var boys = {name: "Billy", age: 5};
+var girls = {name: "Betty", age: 6};
+
+console.log(find_match(boys, girls));
+
+// release 2
+
