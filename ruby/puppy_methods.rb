7833c31b6a34e8e03375cd6781178a486af976c5
Christopher Chan <christopherchan35@gmail.com>
2016-08-15 17:37:01 -0700

    Finished Release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -32,5 +32,37 @@ doggy = Puppy.new
 doggy.fetch("ball")
 doggy.speak(3)
 doggy.roll_over
-p doggy.dog_years(10)
+puts doggy.dog_years(10)
 doggy.plays_dead
+puts "\n"
+
+class Kitten
+
+  def initialize
+    puts "Initializing kitten instance"
+  end
+
+  def hiss(cat_name, human_name)
+    puts "#{cat_name} hissed at #{human_name}"
+  end
+
+  def puke
+    puts "*pukes on rug*"
+  end
+
+end
+
+cat_array = []
+
+50.times do
+  cat_array << Kitten.new
+end
+
+names = ["Bob", "Sally", "Nancy", "Steve", "John"]
+cat_names = ["Felix", "Garfield", "Tom", "Kerouac"]
+
+cat_array.each do |kitten|
+  kitten.hiss(cat_names[rand(4)], names[rand(5)])
+  kitten.puke
+  puts "\n"
+end
