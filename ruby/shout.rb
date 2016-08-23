36e23431987cbbd4defc9d3b226ff64820708aa2
Christopher Chan <christopherchan35@gmail.com>
2016-08-22 19:32:28 -0700

    Shout seemed to have a change

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -22,11 +22,11 @@ class Teacher
 end
 
 mr_boss = Boss.new
-p mr_boss.yell_angrily("You're late")
-p mr_boss.yelling_happily("We closed the sale")
+puts mr_boss.yell_angrily("You're late")
+puts mr_boss.yelling_happily("We closed the sale")
 
-puts " "
+puts "\n"
 
 mr_teacher = Teacher.new
-p mr_teacher.yell_angrily("You didn't turn in your homework")
-p mr_teacher.yelling_happily("You got an A")
+puts mr_teacher.yell_angrily("You didn't turn in your homework")
+puts mr_teacher.yelling_happily("You got an A")
