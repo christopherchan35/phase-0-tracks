bf6bfe0de357d66c0ecfcda2afc25e8408adf207
Christopher Chan <christopherchan35@gmail.com>
2016-09-04 23:39:02 -0700

    Create table using heredoc

--- databases/run_tracker/run_tracker.rb
+++ databases/run_tracker/run_tracker.rb
@@ -5,3 +5,13 @@ require 'sqlite3'
 
 db = SQLite3::Database.new("runs.db")
 
+# command to create initial Database
+create_run_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS runs(
+    id INTEGER PRIMARY KEY,
+    number_of_runs INT,
+    distance_ran DECIMAL(3,2)
+    )
+SQL
+
+db.execute(create_run_table)
